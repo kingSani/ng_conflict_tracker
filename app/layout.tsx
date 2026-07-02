138cf44f292bd104936017839a084ebf77aa346d
Ahmed Sani Galadima <112614761+kingSani@users.noreply.github.com>
2026-07-02 10:31:47 +0100

    Remove unused import of useSuspenseInfra

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useSuspenseInfra } from "react";
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
